a0027bd6915b74ee40dc15cf29457ab268e7139e
nageena88 <ram.nageena88@hotmail.com>
2022-07-03 11:41:13 +0530

    remove 2ns call

--- tests/chp3/video1/test_fixtures_start.py
+++ tests/chp3/video1/test_fixtures_start.py
@@ -2,7 +2,32 @@
 import pytest
 
 from scripts import data_processor, map_population_update
-from test_fixtures_end import map_data_location, prep_transform_data
+
+# from test_fixtures_end import map_data_location, prep_transform_data
+
+
+@pytest.fixture(scope="session")
+def map_data_location():
+    yield "tests/resources/cities/clean_map.csv"
+
+
+@pytest.fixture(scope="function")
+def prep_transform_data(map_data_location):
+    population_dict = {
+        "Andorra": 77142,
+        "Argentina": 44780677,
+        "Cape Verde": 546388,
+        "Germany": 83670653,
+        "Greece": 10473455,
+        "India": 1366417754,
+        "Japan": 128860301,
+        "Morocco": 36471769,
+        "Senegal": 16296364,
+        "United States": 329064917,
+    }
+    data = data_processor.csv_reader(map_data_location)
+    data_to_transform = map_population_update.MapData(data, False)
+    yield data_to_transform, population_dict
 
 
 def test_data_population_update(prep_transform_data):
@@ -21,7 +46,6 @@ def test_data_population_no_update(prep_transform_data):
     sad path test: We don't want tha data transformed twice.
     """
     data_to_transform, population_dict = prep_transform_data
-    data_to_transform.add_population(population_dict)
     data_to_transform.add_population(population_dict)
     with pytest.raises(Exception) as ext:
         data_to_transform.add_population(population_dict)
